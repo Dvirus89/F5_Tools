d0e93213711f6804663c539023bcc0f299fbb91b
Dvir Perets <Dvirus89@gmail.com>
2023-06-09 20:39:23 +0300

    fix path

--- activate_license.sh
+++ activate_license.sh
@@ -3,10 +3,10 @@
 license_pool_path="/var/tmp/license_pool.txt"  # Replace with the actual path to your file
 
 # Read the last line of the file and store it in a variable
-last_line=$(tail -n 1 "$file_path")
+last_line=$(tail -n 1 "$license_pool_path")
 
 # Remove the last line from the file
-sed -i '$d' "$file_path"
+sed -i '$d' "$license_pool_path"
 
 echo "Fixing management default route"
 tmsh modify sys management-route default { gateway 10.1.1.1 }
