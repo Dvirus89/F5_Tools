6ce702ecf16bd2429b552a3d422a506101c18d48
root <root@ip-10-1-1-9.us-west-2.compute.internal>
2023-09-20 19:52:31 +0000

    reporting and feedback

--- attack-tests/test-waf.py
+++ attack-tests/test-waf.py
@@ -1,4 +1,5 @@
 import requests
+import urllib3
 import os
 import json
 import re
@@ -6,15 +7,27 @@ import re
 
 ### Written by Asaf Sahar ###
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+BLOCK_RESPONSE_REGEX_PATTERN=r"Your support ID is- (.*) <br/>" 
+TARGET_ENDPOINT="https://asafdemo-partner.emea-ent.f5demos.com"
+TOP_COLUMN="Platform, Attack File Path, Request Allowed, Request Blocked, Support ID (if exists)\n"
+ENTRY_COLUMN="{platform}, {attack_file_path}, {allowed}, {blocked}, {supportid}\n"
+OUTPUT_PATH = "report.csv"
+PLATFORM="BIGIP"
+
+def write_file(path, content):
+        with open(path, 'a', encoding='utf-8') as f:
+                    f.write(content)
+
+write_file(OUTPUT_PATH, TOP_COLUMN)
 for attack_vector_dir in os.listdir('attacks/'):
+    print(f"Proccesing attack vector: {attack_vector_dir}...")
     for attack_file in os.listdir(f"attacks/{attack_vector_dir}/"):
         attack_file_path = f"attacks/{attack_vector_dir}/{attack_file}" 
-        print(attack_file_path)
-        input("wait")
-        REGEX_PATTERN=r"Your support ID is- (.*) <br/>" # regex alternative for capture group [A-Z0-9\-]{33}
+        print(f"\tProccesing attack file: {attack_file_path}...")
         # Read the JSON data from the external file
-        with open(file_path, 'r') as file:
+        with open(attack_file_path, 'r') as file:
             request_data_list = json.load(file)
 
         # Loop through each request option and send the requests
@@ -25,22 +38,26 @@ for attack_vector_dir in os.listdir('attacks/'):
             url = request_data.get("url", "")
             headers = request_data.get("headers", {})
             data = request_data.get("data", {})
+            print(f"\t\tProccesing attack #{str(count_allow+count_block)}...")
 
-            full_url = f"https://asafdemo-partner.emea-ent.f5demos.com{url}"
+            full_url = f"{TARGET_ENDPOINT}{url}"
             response = ""
             if method == "GET":
-                response = requests.get(full_url, headers=headers)
+                response = requests.get(full_url, headers=headers, verify=False)
             elif method == "POST":
-                response = requests.post(full_url, headers=headers, json=data)
+                response = requests.post(full_url, headers=headers, json=data, verify=False)
             else:
                 print("Unsupported HTTP method")
 
-            matchs = re.findall(REGEX_PATTERN, response.text)
+            matchs = re.findall(BLOCK_RESPONSE_REGEX_PATTERN, response.text)
             for match in matchs:
                 print (match)
             if matchs.__len__() > 0:
                 count_block += 1
+                print("\t\t\tblocked")
+                write_file(OUTPUT_PATH, ENTRY_COLUMN.format(platform=PLATFORM, attack_file_path=attack_file_path, allowed="no", blocked="yes", supportid=match)) 
             else:
                 count_allow += 1
-            #print(f"sum, allow={count_allow}, blocked={count_block}")
-    print(f"sum, allow={count_allow}, blocked={count_block}")
+                print("\t\t\tpassed")
+                write_file(OUTPUT_PATH, ENTRY_COLUMN.format(platform=PLATFORM, attack_file_path=attack_file_path, allowed="yes", blocked="no", supportid="")) 
+    print(f"summary for {attack_file}: allow={count_allow}, blocked={count_block}")
